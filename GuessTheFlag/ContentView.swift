57ebb607a0378f7a97cf40aae551208e3616ddbe
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-05-14 01:05:51 -0700

    Updated to improve accessibility

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -17,6 +17,20 @@ struct ContentView: View {
     
     @State private var selectedFlag: Int? = nil
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+    
     var body: some View {
         ZStack {
             RadialGradient(stops: [
@@ -45,6 +59,7 @@ struct ContentView: View {
                                 flagTapped(number)
                         } label: {
                             FlagImage(country: countries[number])
+                                .accessibilityLabel(labels[countries[number], default: "Unknown Flag"])
                                 .rotation3DEffect(.degrees(selectedFlag == number ? 360 : 0), axis: (x: 0, y: 1, z: 0))
                                 .opacity(showingScore && number != selectedFlag ? 0.25 : 1.0)
                                 .blur(radius: showingScore && number != selectedFlag ? 4 : 1)
